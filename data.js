f57144e68e25f99f9359ed68d3fb089cb7ec776f
D99-1 <79247409+D99-1@users.noreply.github.com>
2021-09-05 11:55:27 +1000

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const songs=[
+    {
+    title:"Believer",
+    artist:"Imagine Dragons",
+    image:require('./Art_980015.jpeg'),
+    id:'1',
+  },
+  {
+    title:"..",
+    artist:'...',
+    image:require('./500x500.jpeg'),
+    id:'2',
+  },
+];
+
+export default songs;
